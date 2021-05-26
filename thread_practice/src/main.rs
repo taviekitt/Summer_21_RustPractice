3a122b8533d6138028b3fe13af06969436a2e0c8
Tavie Kittredge <taviekit@gmail.com>
2021-05-26 10:59:46 -0400

    After convo changes

--- thread_practice/src/main.rs
+++ thread_practice/src/main.rs
@@ -11,38 +11,48 @@ const THREAD_NUM: u32 = 16;
 fn main() {
     let stopper = Arc::new(Mutex::new(0));
     let mut handles = vec![];
-    let mut my_Hash: HashMap<Mutex<i32>, Mutex<String>> = HashMap::new();
+    let mutable_hash: Arc<Mutex<HashMap<i32, String>>> = Arc::new(Mutex::new(HashMap::new()));
 
     {
         let stopper = Arc::clone(&stopper);
-        let s_handle = thread::spawn(move || {
+        let stopper_handle = thread::spawn(move || {
                 thread::sleep(Duration::from_millis(10000));
-                let mut num = stopper.lock().unwrap();
-                *num = 1;
+                let mut update = stopper.lock().unwrap();
+                *update = 1;
             });
-        handles.push(s_handle);
+        handles.push(stopper_handle);
     }   
 
    for _ in 0..THREAD_NUM {
         let stopper = Arc::clone(&stopper);
-        let mut my_Hash = &my_Hash;
-        let handle = thread::spawn(move || {
+        let my_mutable_hash = Arc::clone(&mutable_hash);
+        let general_handle = thread::spawn(move || {
             while *stopper.lock().unwrap() == 0 {
-                let rand1: Mutex<i32> = Mutex::new(rand::thread_rng().gen_range(1..101));
-                let rand2: Mutex<i32> = Mutex::new(rand::thread_rng().gen_range(1..101));
-                let rand3: Mutex<i32> = Mutex::new(rand::thread_rng().gen_range(1..101));
-               
-                my_Hash.insert(rand1, Mutex::new("insert"));
-                my_Hash.remove(rand2.lock().unwrap());
-                if my_Hash.contains_key(rand3.lock().unwrap()) {
-                    println!("{} has value {}", &rand3, my_Hash.get(&rand3.lock().unwrap()));
+                let rand1 = rand::thread_rng().gen_range(1..101);
+                let rand2 = rand::thread_rng().gen_range(1..101);
+                let rand3 = rand::thread_rng().gen_range(1..101);
+
+                let mut locked_hash = my_mutable_hash.lock().unwrap();
+                locked_hash.insert(rand1, String::from("insert"));
+                locked_hash.remove(&rand2);
+                if locked_hash.contains_key(&rand3) {
+                    println!("{} has value {:?}", &rand3, locked_hash.get(&rand3));
+
+                } else {
+                    println!("The hash does not contain the key {}", &rand3);
                 }
-            }u<&|>
+            }
+    
         });
-        handles.push(handle);
+        handles.push(general_handle);
     }
 
     for handle in handles {
         handle.join().unwrap();
+    }
+    let print_hash = mutable_hash.lock().unwrap();
+    println!("The final hash table is:");
+    for key in print_hash.keys(){
+        println!("{},{:?}", key, print_hash.get(key));
     }
 }
---
+++ thread_practice/target/debug/thread_practice.d
@@ -0,0 +1,1 @@
+/Users/taviekittredge/git_proj/Summer_21/thread_practice/target/debug/thread_practice: /Users/taviekittredge/git_proj/Summer_21/thread_practice/src/main.rs
