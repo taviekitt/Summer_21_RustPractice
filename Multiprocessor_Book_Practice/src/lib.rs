af8f842bdee11ab7cb1f54d8cd2c314c467c34a4
Tavie Kittredge <taviekit@gmail.com>
2021-05-28 16:23:28 -0400

    push

--- Multiprocessor_Book_Practice/src/lib.rs
+++ Multiprocessor_Book_Practice/src/lib.rs
@@ -1,13 +1,9 @@
 //implementation of a set of non-negative numbers
 
-use std::sync::{Mutex};
-use std::num::ParseIntError;
-use std::io;
-use std::clone::Clone;
+use std::sync::{Arc, Mutex};
 
-
-const SET_MIN: i32 = -1;
-const SET_MAX: i32 = 2147483647;
+const SET_MIN: i32 = std::i32::MIN; //make sure to go in and make can't be deleted
+const SET_MAX: i32 = std::i32::MAX; //or made
 
 fn hash_function(value: &i32) -> i32{ //placeholder hash function
     let key = *value;//key = value in all cases
@@ -17,11 +13,11 @@ fn hash_function(value: &i32) -> i32{ //placeholder hash function
 struct Node {
     value: i32,
     key: i32,
-    next: Option<Box<Node>>
+    next: Option<Arc<Node>>
 }
 
 impl Node{
-    pub fn new(value: i32, next_node: Option<Box<Node>>) -> Self{
+    pub fn new(value: i32, next_node: Option<Arc<Node>>) -> Self{
         if (value < 0) | (value > 2147483646) {
             panic!("Value {} is out of the range of the set.", value);
         }
@@ -34,39 +30,55 @@ impl Node{
 }
 
 pub struct PosIntSet {
-    head: Node,
+    head: Option<Arc<Node>>,
+    lock: Mutex<i32>,
 }
 
 impl PosIntSet {
     pub fn new() -> Self {
-        let tail = Box::new(Node::new(SET_MAX, None));
+        let tail = Ok(Arc::new(Node::new(SET_MAX, None)));
         Self {
-            head: Node::new(SET_MIN, Some(tail)),
+            head: Ok(Arc::new(Node::new(SET_MIN, Some(tail))),
+            lock: Mutex::new(0),
         }
     }
+}
+
+impl PosIntSet {
     pub fn add(set: PosIntSet, value: i32) -> bool {
         let key = hash_function(&value);
-        let locked_set = Mutex::new(set).lock().unwrap();
-        let mut prev = &locked_set.head;
-        let mut curr = match &prev.next {
-            Some(node_pointer) => &*node_pointer, //what types is this
+        set.lock.lock();
+        let mut prev: &Option<Arc<Node>> = &set.head;
+        let mut curr: &Option<Arc<Node>> = match prev {
+            Some(node_pointer) => &node_pointer.next, 
             None => panic!("Head does not point to a node. Where is tail?"),
         };
-        while &curr.key < &key {
-            let local_prev = prev;
-            //let curr = Ok(*curr.clone());
-            let local_prev = curr; //set interior prev to earlier curr
-            let local_curr = local_prev.next; 
+        let mut current_key = match &curr {
+            Some(inside_curr) => inside_curr.key,
+            None => panic!("There should be a key here!"),
+        };
+        while &current_key < &key {
+            prev = curr; //set interior prev to earlier curr
+            let curr: &Option<Arc<Node>> = match curr {
+                Some(curr_pointer) => &curr_pointer.next, 
+                None => panic!("I am tail, but should not be."),
+            };
+            current_key = match &curr {
+                Some(inside_curr) => inside_curr.key,
+                None => panic!("There really should be a key here!"),
+            }; 
         }
-            if key == curr.key {
-                return false;
-            }
-            else {
-                let new_node = Node::new(value, Some(curr));
-                prev.next = Some(Box::new(new_node));
-                return true;
-            }
-        
-    }
-
+        if key == current_key {
+            return false;
+        } else {
+            let new_node: Node = match curr {
+            Some(current) => Node::new(value, Some(current.clone())),
+            None => println!("well this is an odd late problem.")
+        };
+        prev.next = Some(Box::new(new_node)); //very difficult line, maybe use unsafe
+        return true;
+    } 
 }
+
+
+
